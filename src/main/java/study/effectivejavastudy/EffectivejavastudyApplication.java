d32ef32bd00811a7ac4c83fec8d2247acb4fb59f
jemin <jemin03120111@gmail.com>
2022-04-05 19:56:01 +0900

    initial commit

---
+++ src/main/java/study/effectivejavastudy/EffectivejavastudyApplication.java
@@ -0,0 +1,13 @@
+package study.effectivejavastudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffectivejavastudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EffectivejavastudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/study/effectivejavastudy/EffectivejavastudyApplicationTests.java
@@ -0,0 +1,13 @@
+package study.effectivejavastudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EffectivejavastudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
